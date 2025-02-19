82c1855ca4d2eb5ef326218cc7f2193ea1782b69
Abdurahim <abdurahim@MacBook-Air-Abdurahim.local>
2025-02-19 13:42:30 +0500

    level 3 bolsa judayam tez yugurib ketadi

--- server.js
+++ server.js
@@ -1,47 +1,56 @@
 const title = document.querySelector('.title')
-const startButton = document.querySelector('.button')
-let time
-const input = document.querySelector('.time')
-const button = document.querySelector('.button')
 const ball = document.querySelector('.ball')
-const text = document.querySelector('.text')
+const time = document.querySelector('.time')
+const difficult = document.querySelector('.difficult')
+const button = document.querySelector('.button')
+let please = document.querySelector('.please')
+let rectPlease = please.getBoundingClientRect()
 let score = 0
-let first
-startButton.addEventListener('click', () => {
-	if (document.querySelector('.time').value > 0) {
-		first = document.querySelector('.time').value
-		ball.style.display = 'block'
-		input.style.display = 'none'
+let dx, dy
+
+button.addEventListener('click', () => {
+	if (+time.value > 0) {
+		time.style.display = 'none'
+		difficult.style.display = 'none'
 		button.style.display = 'none'
-		time = document.querySelector('.time')
-		score = 0
-		ballClick()
+		ball.style.display = 'block'
+		ballClickRandom()
 		timing()
 	}
 })
+function stop() {
+	title.textContent = `${score} ta bosdingiz`
+	time.style.display = 'block'
+	difficult.style.display = 'block'
+	button.style.display = 'block'
+	ball.style.display = 'none'
+	time.value = 16
+	score = 0
+}
 function timing() {
-	title.textContent = time.value
 	if (time.value > 0) {
-		setTimeout(timing, 1000)
+		title.textContent = `${time.value}`
+		setTimeout(() => {
+			timing()
+		}, 1000)
+	} else {
+		stop()
 	}
-
-	if (time.value == 0) {
-		title.textContent = `vaqt tugadi, sizning natijangiz ${score - 1} ta`
-		ball.style.display = 'none'
-		button.style.display = 'block'
-		text.textContent = 'qayta boshlash'
-		input.style.display = 'block'
-		time.value = +first + 1
-	}
-	time.value--
+	;+time.value--
 }
 
-function ballClick() {
-	let randomRadius = Math.floor(Math.random() * (60 - 30 + 1)) + 30
+ball.addEventListener('click', () => {
+	if (difficult.value > 1) {
+		clearTimeout(timeOutId)
+	}
+	score++
+	ballClickRandom()
+})
+
+function ballClickRandom() {
+	let randomRadius = Math.floor(Math.random() * 31) + 30
 	ball.style.height = `${randomRadius}px`
 	ball.style.width = `${randomRadius}px`
-	score++
-
 	let randomLeft = Math.floor(Math.random() * 101)
 	let randomTop = Math.floor(Math.random() * 101)
 	if (randomLeft > 85) {
@@ -52,7 +61,40 @@ function ballClick() {
 	}
 	ball.style.left = `${randomLeft}%`
 	ball.style.top = `${randomTop}%`
+	if (difficult.value > 1) {
+		timeOutId = setTimeout(() => {
+			ballClickRandom()
+		}, 1000)
+	}
+	if (difficult.value > 2) {
+		moveBall()
+	}
 }
-ball.addEventListener('click', () => {
-	ballClick()
-})
+
+function moveBall() {
+	let angle = Math.random() * 2 * Math.PI
+	let speed = 2
+
+	dx = Math.cos(angle) * speed
+	dy = Math.sin(angle) * speed
+
+	animate()
+}
+
+function animate() {
+	let rectBall = ball.getBoundingClientRect()
+	let newX = ball.offsetLeft + dx
+	let newY = ball.offsetTop + dy
+
+	if (newX < 0 || newX + rectBall.width > rectPlease.width) {
+		dx = -dx
+	}
+	if (newY < 0 || newY + rectBall.height > rectPlease.height) {
+		dy = -dy
+	}
+
+	ball.style.left = `${ball.offsetLeft + dx}px`
+	ball.style.top = `${ball.offsetTop + dy}px`
+
+	requestAnimationFrame(animate)
+}
